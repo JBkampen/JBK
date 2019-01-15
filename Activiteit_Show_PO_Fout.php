79bc4330b4ce4fbd19b78dfca0d732931a509b83
JBkampen <46655661+JBkampen@users.noreply.github.com>
2019-01-15 19:35:42 +0100

    Delete Activiteit_Show_PO_Fout.php

--- Activiteit_Show_PO_Fout.php
+++
@@ -1,7 +0,0 @@
-<?php 
-	$action="Show_FOUT";
-	$activiteit = new Activiteit($action, $this->locatie, SHOW_FORM);
-	$activiteit->formNaam="Show_PurchaseOrder_Fout.php";
-	$activiteit->naam="Toon foutmelding";
-	$activiteit->button="Toon Scherm";
-?>
